3971f717507f29517657d0166d564aa6b79d2b79
Georgina Serra Oliveras <ls24337@salleurl.edu>
2018-09-23 10:59:55 +0200

    Es modifiquen les propietats que rep el Modal

--- src/js/components/Galeria.js
+++ src/js/components/Galeria.js
@@ -1,41 +1,29 @@
 import React, { Component } from 'react';
 import Modal from "./Modal";
+import { API_KEY } from "../../config";
 import './Galeria.css';
 
-const API_URL_GET_PHOTOS = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=1ffb89f5dcaa0f0c96c86fd180deade0&gallery_id=117615905-72157695735361740&extras=description%2C+media&format=json&nojsoncallback=1";
+const GALLERY_ID = "117615905-72157695735361740";
+const API_URL_GET_PHOTOS = `https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${API_KEY}&gallery_id=${GALLERY_ID}&extras=description%2C+media&format=json&nojsoncallback=1`;
 
 const midaFotos = "m";
 
-const photoDescription = description => <p>{description}</p>;
-// const photoBox = foto => <div key={foto.id} className="gallery-item">
-//   <h3>{foto.title}</h3>
-//   <img src={`https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_${midaFotos}.jpg`} alt={foto.title} />
-//   {foto.description && foto.description._content ? <p>foto.description._content</p> : null}
-// </div>;
-
-{/* <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="200px" height="200px">
-      <rect x="0" y="0" width="100%" height="100%" fill="none" stroke="black"/>
-      <circle cx="100" cy="100" r="50" style="stroke: black; fill: red;"/>
-    </svg> */}
-
 const photoBox = foto => {
-  const description = foto.description && foto.description._content ? <div className="module line-clamp"><p>{foto.description._content}</p></div> : null;
+  const description = foto.description && foto.description._content ? <p>{foto.description._content}</p> : null;
   const imageUrl = `https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_${midaFotos}.jpg`;
-  return <div key={foto.id} className="gallery-item">
-    <div className="img-overlay-wrap">
-      <svg fill="#f55" style={{color: foto.media === "video" ? "blue" : "red"}} viewBox="0 3 60 20">
-        <path d="M 0.65359477,1.3817905 C 60.201925,8.44316 121.92863,11.583451 175.81699,28.832771 l 0.6536,-28.10457531 z"></path>
-      </svg>
-      <img src={imageUrl} alt={foto.title} />
-    </div>
-    <h3>{foto.title}</h3>
-    {/* <video width="320" height="240" controls>
-      <source src="source={`http://www.flickr.com/apps/video/stewart.swf?v=49235&photo_id=${foto.id}&photo_secret=${foto.secret}`}" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video> */}
-    {description}
-    <Modal photoId={foto.id} media={foto.media} />
-  </div>;
+  return <div>
+      <div key={foto.id} className="gallery-item">
+        <div className="img-overlay-wrap">
+          <svg style={{ fill: foto.media === "video" ? "5690F7" : "f55" }} viewBox="0 3 60 20">
+            <path d="M 0.65359477,1.3817905 C 60.201925,8.44316 121.92863,11.583451 175.81699,28.832771 l 0.6536,-28.10457531 z"></path>
+          </svg>
+          <img src={imageUrl} alt={foto.title} />
+        </div>
+        <h3>{foto.title}</h3>
+        {description}
+      </div>
+      <Modal media={foto.media} photo={{ id: foto.id, title: foto.title, description: foto.description && foto.description._content ? foto.description._content : "" }} />
+    </div>;
 }
 
 class Galeria extends Component {
--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -1,28 +1,44 @@
 import React, { Component, Fragment } from 'react';
 import './Modal.css';
+import { API_KEY } from '../../config';
 
-const API_URL_GET_PHOTOS_SIZES = photoId => `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=1ffb89f5dcaa0f0c96c86fd180deade0&photo_id=${photoId}&format=json&nojsoncallback=1`;
+const API_URL_GET_PHOTOS_SIZES = photoId => `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
     // http://www.flickr.com/photos/{user-id|custom-url}/{photo-id}/play/{site|mobile|hd|orig}/{secret|originalsecret}/
+
+const getSourceByParam = (array, parametreCerca) => array.find(size => size.label === parametreCerca).source;
 
 class Modal extends Component {
   state = { visible: false }
   componentDidMount() {
-    const { photoId, media } = this.props;
+    const { photo, media } = this.props;
 
-    fetch(API_URL_GET_PHOTOS_SIZES(photoId))
+    const parametreCerca = media === "video" ? "Video Player" : "Original";
+
+    fetch(API_URL_GET_PHOTOS_SIZES(photo.id))
       .then(response => response.json())
-      .then(myJson => this.setState({...this.state, [media]: myJson.sizes.size[media === "video" ? 10 : 1].source}));
+      .then(myJson => {
+          console.log("myJson: ", myJson);
+          this.setState({...this.state, source: getSourceByParam(myJson.sizes.size,  parametreCerca)});
+        //   console.log(myJson);
+        //   const resultado = myJson.sizes.size.find(size => size.label === parametreCerca);
+        //   console.log("====>", resultado.source);
+      });
+
+    //   .then(myJson => this.setState({...this.state, [media]: myJson.sizes.size[media === "video" ? 10 : 1].source}));
   }
   render() {
-    const { media } = this.props;
+    const { media, photo } = this.props;
     const { visible } = this.state;
     return (
         <Fragment>
             <button type="button" onClick={() => this.setState({ visible: true })}>Click to view</button>
             <div className="modal" style={{display: visible ? "block" : "none"}}>
                 <span className="close" onClick={() => this.setState({ visible: false })}>&times;</span>
-                {media === "video" ? <embed src={this.state.video}></embed> : <img className="modal-content" src={this.state.photo} alt="TODO: cal passar ALT" />}
-                <div id="caption"></div>
+                {media === "video" ? <embed className="modal-content" src={this.state.source}></embed> : <img className="modal-content" src={this.state.source} alt="TODO: cal passar ALT" />}
+                <div id="caption">
+                  <h3>{photo.title}</h3>
+                  <p>{photo.description}</p>
+                </div>
             </div>
         </Fragment>
     );
