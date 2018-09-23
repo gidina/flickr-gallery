ea936109095a94b5e3385809b49cc29c26a960cb
Georgina Serra Oliveras <ls24337@salleurl.edu>
2018-09-23 12:12:35 +0200

    S'utilitza Loader quan les peticions estan en curs

--- src/js/components/Galeria.js
+++ src/js/components/Galeria.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Modal from "./Modal";
+import Loader from "./Loader";
 import { API_KEY } from "../../config";
 import './Galeria.css';
 
@@ -10,8 +11,8 @@ const midaFotos = "m";
 const photoBox = foto => {
   const description = foto.description && foto.description._content ? <p>{foto.description._content}</p> : null;
   const imageUrl = `https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_${midaFotos}.jpg`;
-  return <div>
-      <div key={foto.id} className="gallery-item">
+  return <div key={foto.id}>
+      <div className="gallery-item">
         <div className="img-overlay-wrap">
           <svg style={{ fill: foto.media === "video" ? "5690F7" : "f55" }} viewBox="0 3 60 20">
             <path d="M 0.65359477,1.3817905 C 60.201925,8.44316 121.92863,11.583451 175.81699,28.832771 l 0.6536,-28.10457531 z"></path>
@@ -26,18 +27,24 @@ const photoBox = foto => {
 }
 
 class Galeria extends Component {
-  state = { fotosGaleria: { photos: { photo: [] }} }
+  state = { isLoading: true, fotosGaleria: { photos: { photo: [] }} }
   componentDidMount() {
     fetch(API_URL_GET_PHOTOS)
       .then(response => response.json())
-      .then(json => this.setState({ fotosGaleria: json }));
+      .then(json => this.setState({ isLoading: false, fotosGaleria: json }));
   }
   render() {
-    const { fotosGaleria } = this.state;
+    const { isLoading, fotosGaleria } = this.state;
     return (
-      <div className="gallery">
-        {fotosGaleria.photos.photo.map(photoBox)}
-      </div>
+      <Fragment>
+        { 
+          isLoading ? 
+          <Loader color={"#f55"} /> : 
+          <div className="gallery">
+            {fotosGaleria.photos.photo.map(photoBox)}
+          </div>
+        }
+      </Fragment>
     );
   }
 }
--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -1,46 +1,45 @@
 import React, { Component, Fragment } from 'react';
+import Loader from "./Loader";
 import './Modal.css';
 import { API_KEY } from '../../config';
 
 const API_URL_GET_PHOTOS_SIZES = photoId => `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${API_KEY}&photo_id=${photoId}&format=json&nojsoncallback=1`;
-    // http://www.flickr.com/photos/{user-id|custom-url}/{photo-id}/play/{site|mobile|hd|orig}/{secret|originalsecret}/
 
 const getSourceByParam = (array, parametreCerca) => array.find(size => size.label === parametreCerca).source;
 
 class Modal extends Component {
-  state = { visible: false }
-  componentDidMount() {
+  state = { isLoading: false, visible: false }
+  onClickHandler = () => {
     const { photo, media } = this.props;
-
     const parametreCerca = media === "video" ? "Video Player" : "Original";
 
-    fetch(API_URL_GET_PHOTOS_SIZES(photo.id))
-      .then(response => response.json())
-      .then(myJson => {
-          console.log("myJson: ", myJson);
-          this.setState({...this.state, source: getSourceByParam(myJson.sizes.size,  parametreCerca)});
-        //   console.log(myJson);
-        //   const resultado = myJson.sizes.size.find(size => size.label === parametreCerca);
-        //   console.log("====>", resultado.source);
-      });
+    this.setState({ isLoading: true, visible: true });
 
-    //   .then(myJson => this.setState({...this.state, [media]: myJson.sizes.size[media === "video" ? 10 : 1].source}));
+    fetch(API_URL_GET_PHOTOS_SIZES(photo.id))
+    .then(response => response.json())
+    .then(myJson => this.setState({...this.state, isLoading: false, source: getSourceByParam(myJson.sizes.size,  parametreCerca)}));
   }
-  render() {
+  render = () => {
     const { media, photo } = this.props;
-    const { visible } = this.state;
+    const { isLoading, visible } = this.state;
     return (
-        <Fragment>
-            <button type="button" onClick={() => this.setState({ visible: true })}>Click to view</button>
-            <div className="modal" style={{display: visible ? "block" : "none"}}>
-                <span className="close" onClick={() => this.setState({ visible: false })}>&times;</span>
+      <Fragment>
+        <button type="button" onClick={this.onClickHandler}>Click to view</button>
+        <div className="modal" style={{display: visible ? "block" : "none"}}>
+            <span className="close" onClick={() => this.setState({ visible: false })}>&times;</span>
+            { 
+              isLoading ? 
+              <Loader color={"#5690F7"} /> :
+              <Fragment>
                 {media === "video" ? <embed className="modal-content" src={this.state.source}></embed> : <img className="modal-content" src={this.state.source} alt="TODO: cal passar ALT" />}
                 <div id="caption">
                   <h3>{photo.title}</h3>
                   <p>{photo.description}</p>
                 </div>
-            </div>
-        </Fragment>
+              </Fragment>
+            }
+        </div>
+      </Fragment>
     );
   }
 }
