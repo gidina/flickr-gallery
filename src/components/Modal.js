af14f0764626fdbf592923757361f3f390a4b7e5
Georgina Serra Oliveras <ls24337@salleurl.edu>
2018-09-24 18:11:26 +0200

    Refactor Modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,28 +5,25 @@ import Loader from "./Loader";
 import "./Modal.css";
 import { secondaryColor } from "../config";
 
-const Modal = ({ isLoading, photo, onClose }) => {
-  return (
-    <div className="gallery_item_full">
-      {
-        isLoading ? 
-        <Loader color={secondaryColor} /> : 
-        <Fragment>
-          <span className="close" onClick={() => onClose()}>
-            &times;
-          </span>
-          <div className="box">
-            {
-              photo.isVideo ? <embed src={photo.source} /> : <img src={photo.source} alt="" />
-            }
-            <h3>{photo.title}</h3> - <h4 onClick={() => window.open(photo.redirectURL)}>{photo.username}</h4>
-            <p>{photo.description}</p>
-          </div>
-        </Fragment>
-      }
-    </div>
-  );
-};
+const Modal = ({ isLoading, photo, onClose }) => 
+  <div className="gallery_item_full">
+    {
+      isLoading ? 
+      <Loader color={secondaryColor} /> : 
+      <Fragment>
+        <span className="close" onClick={() => onClose()}>
+          &times;
+        </span>
+        <div className="box">
+          {
+            photo.isVideo ? <embed src={photo.source} /> : <img src={photo.source} alt="" />
+          }
+          <h3>{photo.title}</h3> - <h4 onClick={() => window.open(photo.redirectURL)}>{photo.username}</h4>
+          <p>{photo.description}</p>
+        </div>
+      </Fragment>
+    }
+  </div>;
 
 Modal.propTypes = {
   isLoading: PropTypes.bool.isRequired,
