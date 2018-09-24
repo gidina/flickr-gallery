fb8d49cb582c38ffadf5a68e13434a14a55a9f70
Georgina Serra Oliveras <ls24337@salleurl.edu>
2018-09-24 16:24:21 +0200

    refactor Gallery, nou component GalleryItem

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
-import { redColor, blueColor } from "../config";
 import Pagination from "./Pagination";
 import "./Gallery.css";
+import GalleryItem from "./GalleryItem";
 
 const breakpointDesktop = 1047;
 
@@ -14,7 +14,7 @@ class Gallery extends Component {
     const isDesktopCurrent = window.innerWidth > breakpointDesktop;
 
     if (this.state.isDesktop === isDesktopCurrent) return;
-    
+
     if (isDesktopCurrent) {
       this.setState({
         ...this.state,
@@ -44,50 +44,19 @@ class Gallery extends Component {
     return (
       <Fragment>
         <div className="gallery">
-          {currentPhotos.map(foto => (
-            <div
-              key={foto.id}
-              className="gallery_item"
-              onClick={() => onClickPhoto(foto)}
-            >
-              <span
-                className="gallery_item_preview"
-                onClick={() =>
-                  this.setState({
-                    ...this.state,
-                    isModalOpen: true,
-                    fotoSeleccionada: foto
-                  })
-                }
-              >
-                <button>Click to view</button>
-                <svg
-                  fill={foto.media === "video" ? blueColor : redColor}
-                  className="gallery_top"
-                  viewBox="0 3 60 20"
-                >
-                  <path d="M 0.65359477,1.3817905 C 60.201925,8.44316 121.92863,11.583451 175.81699,28.832771 l 0.6536,-28.10457531 z" />
-                </svg>
-                <img
-                  src={`https://farm${foto.farm}.staticflickr.com/${
-                    foto.server
-                  }/${foto.id}_${foto.secret}_b.jpg`}
-                  alt=""
-                />
-                <span>
-                  <h3>{foto.title}</h3>
-                  <p>{foto.description._content}</p>
-                </span>
-              </span>
-            </div>
-          ))}
+          {
+            currentPhotos.map(photo => 
+              <GalleryItem key={photo.id} onClick={onClickPhoto} photo={photo} />)
+          }
         </div>
-        {isDesktop ? (
+        {
+          isDesktop ? 
           <Pagination
             numItems={photos.length}
             onPageChange={this.onPageChange}
-          />
-        ) : null}
+          /> : 
+          null
+        }
       </Fragment>
     );
   }
