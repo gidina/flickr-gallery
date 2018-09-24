1fe294d716db331b7221d926d96f120a0b6c52ed
Georgina Serra Oliveras <ls24337@salleurl.edu>
2018-09-24 18:10:03 +0200

    Refactor Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,14 +3,10 @@ import PropTypes from 'prop-types';
 
 import "./Loader.css";
 
-const Loader = ({ color }) => {
-  return (
-    <div
-      className="loader"
-      style={{ border: "10px solid #f3f3f3", borderTop: `10px solid ${color}` }}
-    />
-  );
-};
+const Loader = ({ color }) => <div 
+  className="loader" 
+  style={{ border: "10px solid #f3f3f3", borderTop: `10px solid ${color}` }} 
+  />;
 
 Loader.propTypes = {
     color: PropTypes.string.isRequired
