30d8df7639994fde905ccea31c5767703d66eb3e
Georgina Serra Oliveras <ls24337@salleurl.edu>
2018-09-24 16:52:31 +0200

    reset.css deleted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-// import './reset.css';
 
 const wrapper = document.getElementById("root");
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
