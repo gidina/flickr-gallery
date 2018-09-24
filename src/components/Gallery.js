43d0ad799dcbf5c030d3624dfc696c1cd47f52b5
Georgina Serra Oliveras <ls24337@salleurl.edu>
2018-09-24 13:29:22 +0200

    S'aplica la paginació a la galeria

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,33 +1,65 @@
-import React from 'react';
-import { redColor, blueColor } from "../../config";
+import React, { Component, Fragment } from "react";
+import { redColor, blueColor } from "../config";
+import Pagination from "./Pagination";
 import "./Gallery.css";
 
 const midaFotos = "b";
 
-const Gallery = ({photos, onClickPhoto}) => {
+class Gallery extends Component {
+  state = { currentPhotos: this.props.photos };
+  onPageChange = (startIndex, endIndex) => {
+    const { photos } = this.props;
+    const currentPhotos = photos.slice(startIndex, endIndex + 1);
+    this.setState({ ...this.state, currentPhotos });
+  };
+  render = () => {
+    const { photos, onClickPhoto } = this.props;
+    const { currentPhotos } = this.state;
     return (
+      <Fragment>
         <div className="gallery">
-
-        
-            {
-                photos.map(foto => 
-                    <div key={foto.id} className="gallery_item" onClick={() => onClickPhoto(foto)}>
-                <span className="gallery_item_preview" onClick={() => this.setState({ ...this.state, isModalOpen: true, fotoSeleccionada: foto })}>
-                    <button>Click to view</button>
-                    <svg fill={foto.media === "video" ? blueColor : redColor} className="gallery_top" viewBox="0 3 60 20">
-                        <path d="M 0.65359477,1.3817905 C 60.201925,8.44316 121.92863,11.583451 175.81699,28.832771 l 0.6536,-28.10457531 z"></path>
-                    </svg>
-                    <img src={`https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}_${midaFotos}.jpg`} alt="" />
-                    <span>
-                        <h3>{foto.title}</h3>
-                        <p>{foto.description._content}</p>
-                    </span>
+          {currentPhotos.map(foto => (
+            <div
+              key={foto.id}
+              className="gallery_item"
+              onClick={() => onClickPhoto(foto)}
+            >
+              <span
+                className="gallery_item_preview"
+                onClick={() =>
+                  this.setState({
+                    ...this.state,
+                    isModalOpen: true,
+                    fotoSeleccionada: foto
+                  })
+                }
+              >
+                <button>Click to view</button>
+                <svg
+                  fill={foto.media === "video" ? blueColor : redColor}
+                  className="gallery_top"
+                  viewBox="0 3 60 20"
+                >
+                  <path d="M 0.65359477,1.3817905 C 60.201925,8.44316 121.92863,11.583451 175.81699,28.832771 l 0.6536,-28.10457531 z" />
+                </svg>
+                <img
+                  src={`https://farm${foto.farm}.staticflickr.com/${
+                    foto.server
+                  }/${foto.id}_${foto.secret}_${midaFotos}.jpg`}
+                  alt=""
+                />
+                <span>
+                  <h3>{foto.title}</h3>
+                  <p>{foto.description._content}</p>
                 </span>
-                </div>)
-            }
-                </div>
-
+              </span>
+            </div>
+          ))}
+        </div>
+        <Pagination numItems={photos.length} onPageChange={this.onPageChange} />
+      </Fragment>
     );
+  };
 }
 
 // Gallery.protoTypes = {
