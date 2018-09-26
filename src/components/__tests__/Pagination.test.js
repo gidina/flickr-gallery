309424587be7ba2282384db6509f1242fa2bfb83
Georgina Serra Oliveras <gserra@meteo.cat>
2018-09-26 09:33:43 +0200

    Pagination unit tests

---
+++ src/components/__tests__/Pagination.test.js
@@ -0,0 +1,12 @@
+
+// props
+//   numItems: PropTypes.number.isRequired,
+//   onPageChange: PropTypes.func.isRequired
+
+// const defaultActive = 0;
+// const numItemsPage = 8;
+
+test.skip("per defecte es mostren les primeres 8 fotos (onPageChange es crida amb 0, 7) i la primera pàgina està en red (classe 'active')", () => {});
+test.skip("faig click a una pàgina i aquesta passa a ser activa (onPageChange es crida correctament*)", () => {});
+test.skip("faig click a << i la pàgina activa passa a ser l'anterior (excepte si estic a la primera)", () => {});
+test.skip("faig click a >> i la pàgina activa passa a ser la següent (excepte si estic a l'última)", () => {});
