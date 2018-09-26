e35b95669db147d90697658c308f0df29b153d13
Georgina Serra Oliveras <gserra@meteo.cat>
2018-09-26 09:33:25 +0200

    App unit tests

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,5 @@
+test.skip("Si isLoading és true, es mostra un Loader de color red", () => {});
+test.skip("Si no s'ha seleccionat cap foto, no es mostra el Modal", () => {});
+test.skip("Si s'ha seleccionat una foto, es mostra el Modal", () => {});
+test.skip("Quan es selecciona una foto, la propietat isLoadingModal es posa a true", () => {});
+test.skip("Si es rep l'onClose del modal, es deselecciona la foto", () => {});
